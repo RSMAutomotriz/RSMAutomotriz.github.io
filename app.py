1c398c90371a140178206145232a758e7d17914a
RSMAutomotriz <rsmautomotriz.ptomontt@gmail.com>
2025-01-03 10:27:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -473,6 +473,8 @@ def eliminar_imagen(matricula, filename):
         print(f"Error al eliminar imagen: {str(e)}")  # Para debugging
         return jsonify({'success': False, 'message': f'Error al eliminar la imagen: {str(e)}'}), 500
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=port)

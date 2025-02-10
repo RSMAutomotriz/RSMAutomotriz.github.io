819edfe2f36fc489698e7a5cd71d87f602939a4e
RSMAutomotriz <rsmautomotriz.ptomontt@gmail.com>
2025-02-10 10:34:48 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-12345'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://username:password@localhost/rsm_automotriz'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://usuario:contraseña@localhost/rsm_automotriz'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'

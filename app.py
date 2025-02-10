0b754fb28edf0167cc296b39c84044589b18518d
RSMAutomotriz <rsmautomotriz.ptomontt@gmail.com>
2025-02-10 10:40:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 # Cambio en la inicialización de SQLAlchemy
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-db = SQLAlchemy(app, engine=engine)
+db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -149,4 +151,8 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

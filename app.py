4d708ba4f0e48130e7dff8f350e8d02667e74aee
RSMAutomotriz <rsmautomotriz.ptomontt@gmail.com>
2025-02-10 10:47:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -146,8 +146,9 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
+    # Crear tablas si no existen
     with app.app_context():
         db.create_all()
-        
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
